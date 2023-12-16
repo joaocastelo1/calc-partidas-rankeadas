2ef0734f148d0a4c81bcbba32865332d96d25b9d
joaocastelo1 <castelopiaui7@gmail.com>
2023-12-15 23:32:19 -0300

    calc rakeadas

---
+++ script.js
@@ -0,0 +1,32 @@
+// Função para calcular o nível do jogador com base nas vitórias e derrotas
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+  
+    if (vitorias < 10) {
+      nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+    } else {
+      nivel = "Imortal";
+    }
+  
+    // Retornar o resultado
+    return `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+  }
+  
+  // Exemplo de uso
+  const vitoriasDoHeroi = 95;
+  const derrotasDoHeroi = 20;
+  
+  const resultado = calcularNivel(vitoriasDoHeroi, derrotasDoHeroi);
+  console.log(resultado);
+  
